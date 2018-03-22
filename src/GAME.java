64016d20f8ead0a0131c160955fb128eea7b904b
Paul Arnaud <paul.arnaud@etu.umontpellier.fr>
2018-03-22 14:32:04 +0100

    Game implementation

--- src/GAME.java
+++ src/GAME.java
@@ -1,4 +1,26 @@
 
 public class GAME {
+	public PLAYER ActivePlayer;
+	public PLAYER OppositePlayer;
+	
+	public PLAYER getActivePlayer() {
+		return ActivePlayer;
+	}
+	public void setActivePlayer(PLAYER activePlayer) {
+		ActivePlayer = activePlayer;
+	}
+	public PLAYER getOppositePlayer() {
+		return OppositePlayer;
+	}
+	public void setOppositePlayer(PLAYER oppositePlayer) {
+		OppositePlayer = oppositePlayer;
+	}
+	
+	public void changePlayer() {
+		PLAYER TempPlayer;
+		TempPlayer = this.ActivePlayer;
+		this.ActivePlayer = this.OppositePlayer;
+		this.OppositePlayer = TempPlayer;
+	}
 
 }
