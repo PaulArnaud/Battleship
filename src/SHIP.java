0da1d0e50e861888828f33fa9a176e2de85940fe
Paul Arnaud <paul.arnaud@etu.umontpellier.fr>
2018-03-22 13:04:09 +0100

    Ship implementation

--- src/SHIP.java
+++ src/SHIP.java
@@ -1,4 +1,48 @@
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ListIterator;
+
 
 public class SHIP {
+    private int row;
+    private int col;
+    private List localisation;
+    
+	public int getRow() {
+		return row;
+	}
+	public void setRow(int row) {
+		this.row = row;
+	}
+	public int getCol() {
+		return col;
+	}
+	public void setCol(int col) {
+		this.col = col;
+	}
+	public List getLocalisation() {
+		return localisation;
+	}
+	/*public void setLocalisation(int row,int col) {
+		
+		this.localisation = localisation;
+	}*/
+	
+	public boolean  isHit(int pos){
+		if (this.localisation.contains(pos))
+			return true;
+		else 
+			return false;
+	}
+	
+	
+	public boolean isDestroyed(){
+		if (this.localisation.isEmpty())
+			return true;
+		else
+			return false;
+	}
 
+    
+    
 }
